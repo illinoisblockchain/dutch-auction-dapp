2f2ac9f6a3b696f0a67354ee8cf176ad9bed9c55
Ron Gierlach <rongierlach@gmail.com>
2017-08-15 11:55:35 -0500

    update deploy contract form

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,67 @@
 // Supports Mist, and other wallets that provide 'web3'.
 if (typeof web3 !== 'undefined') {
   // Use the Mist/wallet/Metamask provider.
-  console.log('web3 found')
+  console.log('Injected web3 detected.')
   window.web3 = new Web3(web3.currentProvider);
 } else {
   // Your preferred fallback.
+  console.log('No web3 instance injected, using Local web3.')
   window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 }
 
-var deployedContract;
+function buildContractUI(deployedContract) {
+  var address = deployedContract.address
+  console.log({deployedContract, address})
+}
 
 $(function() {
   $(window).load(function() {
-    var $contractForm = $('#contract-form');
-    var $contractUI = $('#contract-ui');
+    var $contractFormsContainer = $('#contract-forms-container');
+    var $accessContractForm = $('#access-contract-form');
+    var $deployContractForm = $('#deploy-contract-form');
+    var $contractUIContainer = $('#contract-ui-container');
 
-    $contractForm.submit(function(e) {
+    $accessContractForm.submit(function(e) {
       e.preventDefault();
-      var abi = $('#abi-input').val();
-      var address = $('#address-input').val();
-      var contract = web3.eth.contract(JSON.parse(abi));
-      deployedContract = contract.at(address);
-      $contractForm.hide();
-      console.log({deployedContract});
+      var abi = JSON.parse($('#access-abi-input').val().trim());
+      var address = $('#access-address-input').val();
+      var contract = web3.eth.contract(abi);
+      var deployedContract = contract.at(address);
+      buildContractUI(deployedContract);
+    });
+
+    $deployContractForm.submit(function(e) {
+      e.preventDefault()
+      var initialPrice = web3.toWei($('#deploy-initialPrice-input').val(), 'ether');
+      var biddingPeriod = Number($('#deploy-biddingPeriod-input').val());
+      var offerPriceDecrement = web3.toWei($('#deploy-offerPriceDecrement-input').val(), 'ether');
+      var testMode = $('#deploy-testMode-input').prop('checked');
+
+      var abi = JSON.parse($('#deploy-abi-input').val().trim());
+      var bytecode = $('#deploy-bytecode-input').val().trim();
+      var gas = Number($('#deploy-gas-input').val());
+
+      // deploy the contract
+      web3.eth.contract(abi).new(
+        initialPrice,
+        biddingPeriod,
+        offerPriceDecrement,
+        testMode,
+        { data: '0x' + bytecode, from: web3.eth.coinbase, gas: gas },
+        function(err, deployedContract) {
+          console.log({err, deployedContract})
+          // check tx hash on the first call (transaction send)
+          if (!deployedContract.address) {
+            // The hash of the transaction, which deploys the contract
+            console.log('txHash', deployedContract.transactionHash)
+          // check address on the second call (contract deployed)
+          } else {
+            // the contract address
+            console.log('address', deployedContract.address);
+            buildContractUI(deployedContract);
+          }
+        }
+      )
     });
   });
 });
